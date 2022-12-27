47483d7f4a82f3b15804926b5740557df9e724dd
abhijitnr <abhijitbiswas8859@gmail.com>
2022-12-28 04:27:36 +0530

    Navbar completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Header from "./components/layout/Header";
+import Home from "./components/home/Home";
+
+import "./styles/app.scss";
+import "./styles/header.scss";
+
 function App() {
-  return <div>APP</div>;
+  return (
+    <Router>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
