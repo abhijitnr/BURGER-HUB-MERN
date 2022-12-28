184ca05deb1da6d737a5f2f486679f7e1f922877
abhijitnr <abhijitbiswas8859@gmail.com>
2022-12-28 15:59:20 +0530

    Contact page completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Header from "./components/layout/Header";
 import Home from "./components/home/Home";
+import Footer from "./components/layout/Footer";
+import Contact from "./components/contact/Contact";
 
 import "./styles/app.scss";
 import "./styles/header.scss";
@@ -9,7 +11,7 @@ import "./styles/home.scss";
 import "./styles/founder.scss";
 import "./styles/menu.scss";
 import "./styles/footer.scss";
-import Footer from "./components/layout/Footer";
+import "./styles/contact.scss";
 
 function App() {
   return (
@@ -19,6 +21,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
 
       {/* Footer */}
